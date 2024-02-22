335984a0e5a12f4fd377c32bbc9260f129671493
Khatiketki <khati.k@northeastern.edu>
2024-02-22 02:23:00 -0800

    git status

--- java_projects/src/main/java/edu/neu/mgen/Oakland.java
+++ java_projects/src/main/java/edu/neu/mgen/Oakland.java
@@ -1,22 +1,20 @@
+package edu.neu.mgen;
 
-public class Test3 {
+public class Oakland {
     public static void main(String[] args) {
         String str = "Oakland";
-
         // Find the length of the string
         int length = str.length();
         System.out.println("Length of the string: " + length);
-
         // Find a character with index 2 in the string
         char charAtIndex2 = str.charAt(2);
         System.out.println("Character at index 2: " + charAtIndex2);
-
         // Extract a substring "land" from str
         String substring = str.substring(3); // index 3 to end
         System.out.println("Substring 'land' from str: " + substring);
-
         // Convert all letters in str to capital letters "OAKLAND"
         String uppercaseStr = str.toUpperCase();
         System.out.println("String in capital letters: " + uppercaseStr);
-    }
+        }
+    
 }
